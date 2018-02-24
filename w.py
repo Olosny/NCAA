9add549f90a255072d3d6cf0bbcd426361c3acc0
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-02-24 19:31:37 +0100

    nouvelle tentative :)

--- w.py
+++ w.py
@@ -6,7 +6,7 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
-# load data
+# --------------------------------- Load data ---------------------------------
 cities = pd.read_csv('./data_w/WCities.csv')
 gamecities = pd.read_csv('./data_w/WGameCities.csv')
 tourney = pd.read_csv('./data_w/WNCAATourneyCompactResults.csv')
@@ -18,99 +18,167 @@ teamspellings = pd.read_csv('./data_w/WTeamSpellings.csv', engine='python')
 teams = pd.read_csv('./data_w/WTeams.csv')
 sub = pd.read_csv('./data_w/WSampleSubmissionStage1.csv')
 
-#%% 1st model : tourney based only
-# based on seed diff
+
+# --------------------------------- Formating ---------------------------------
+# format tourney and regseason results
+def format_results(df):
+    df['Result'] = 1
+    for i, row in df.iterrows():
+        if row['WTeamID'] > row['LTeamID']:
+            df.set_value(i, 'WTeamID', row['LTeamID'])
+            df.set_value(i, 'LTeamID', row['WTeamID'])
+            df.set_value(i, 'Result', 0)
+            df.set_value(i, 'WScore', row['LScore'])
+            df.set_value(i, 'LScore', row['WScore'])
+            if row['WLoc'] == 'H':
+                df.set_value(i, 'WLoc', 'A')
+            if row['WLoc'] == 'A':
+                df.set_value(i, 'WLoc', 'H')
+    df.rename(columns={'WTeamID':'T1ID','LTeamID':'T2ID'}, inplace=True)
+    df.rename(columns={'WScore':'T1Score','LScore':'T2Score'}, inplace=True)
+
+format_results(tourney)
+format_results(regseason)
+
+# merge seeds & tourney results
 def seed_to_int(seed):
     s_int = int(seed[1:3])
     return s_int
 
 seeds['Seed'] = seeds['Seed'].apply(seed_to_int)
+t1seeds = seeds.rename(columns={'TeamID':'T1ID', 'Seed':'T1Seed'})
+t2seeds = seeds.rename(columns={'TeamID':'T2ID', 'Seed':'T2Seed'})
+tourney = pd.merge(left=tourney, right=t1seeds, how='left', on=['Season', 'T1ID'])
+tourney = pd.merge(left=tourney, right=t2seeds, how='left', on=['Season', 'T2ID'])
+tourney['SeedDiff'] = tourney['T1Seed'] - tourney['T2Seed']
 
-wseeds = seeds.rename(columns={'TeamID':'WTeamID', 'Seed':'WSeed'})
-lseeds = seeds.rename(columns={'TeamID':'LTeamID', 'Seed':'LSeed'})
-seeds = pd.merge(left=tourney, right=wseeds, how='left', on=['Season', 'WTeamID'])
-seeds = pd.merge(left=seeds, right=lseeds, how='left', on=['Season', 'LTeamID'])
-seeds['SeedDiff'] = seeds['WSeed'] - seeds['LSeed']
-seeds['Result'] = 1
-seeds.drop(['DayNum','WScore','LScore','WLoc','NumOT','WSeed','LSeed'], axis=1, inplace=True)
+# extract stats from reg season
+# pts scored/taken ; w/l ...
+t1pts = regseason.groupby(['Season','T1ID'])['T1Score'].sum().reset_index()
+t1pts.rename(columns={'T1ID':'TeamID','T1Score':'T1PtsSc'}, inplace = True)
+t1pts['T1PtsTk'] = regseason.groupby(['Season','T1ID'])['T2Score'].sum().reset_index()['T2Score']
+t1pts['T1ct'] = regseason.groupby(['Season','T1ID'])['T1Score'].count().reset_index()['T1Score']
+t1pts['T1Wins'] = regseason.groupby(['Season','T1ID'])['Result'].sum().reset_index()['Result']
 
-for i, row in seeds.iterrows():
-    if row['WTeamID'] > row['LTeamID']:
-        wtid = row['WTeamID']
-        row['WTeamID'] = row['LTeamID']
-        row['LTeamID'] = wtid
-        row['SeedDiff'] *= -1
-        row['Result'] = 0
+t2pts = regseason.groupby(['Season','T2ID'])['T2Score'].sum().reset_index()
+t2pts.rename(columns={'T2ID':'TeamID','T2Score':'T2PtsSc'}, inplace = True)
+t2pts['T2PtsTk'] = regseason.groupby(['Season','T2ID'])['T1Score'].sum().reset_index()['T1Score']
+t2pts['T2ct'] = regseason.groupby(['Season','T2ID'])['T2Score'].count().reset_index()['T2Score']
+t2pts['T2Wins'] = regseason.groupby(['Season','T2ID'])['Result'].sum().reset_index()['Result']
+t2pts['T2Wins'] = t2pts['T2ct'] - t2pts['T2Wins']
 
-print(seeds)
+pts = pd.merge(left=t1pts, right=t2pts, how='outer', on=['Season', 'TeamID'])
+pts.fillna(0, inplace=True)
 
-wins = seeds.loc[:,['Season','SeedDiff']]
-wins['Result'] = 1
-losses = seeds.loc[:,['Season','SeedDiff']]
-losses['SeedDiff'] *= -1
-losses['Result'] = 0
-data = pd.concat([wins, losses]).reset_index(drop=True)
+pts['MPtsSc'] = (pts['T1PtsSc'] + pts['T2PtsSc']) / (pts['T1ct'] + pts['T2ct'])
+pts['MPtsTk'] = (pts['T1PtsTk'] + pts['T2PtsTk']) / (pts['T1ct'] + pts['T2ct'])
+pts['Wratio'] = (pts['T1Wins'] + pts['T2Wins']) / (pts['T1ct'] + pts['T2ct'])
+pts['Wdelta'] = 2*(pts['T1Wins'] + pts['T2Wins']) - (pts['T1ct'] + pts['T2ct'])
+pts.drop(['T1PtsSc','T1PtsTk','T2PtsSc','T2PtsTk','T1ct','T2ct','T1Wins','T2Wins'], axis=1, inplace=True)
+
+t1pts = pts.rename(columns={'TeamID':'T1ID', 'MPtsSc':'T1MPtsSc', 'MPtsTk':'T1MPtsTk', 'Wratio':'T1Wratio', 'Wdelta':'T1Wdelta'})
+t2pts = pts.rename(columns={'TeamID':'T2ID', 'MPtsSc':'T2MPtsSc', 'MPtsTk':'T2MPtsTk', 'Wratio':'T2Wratio', 'Wdelta':'T2Wdelta'})
+regseason = pd.merge(left=regseason, right=t1pts, how='left', on=['Season', 'T1ID'])
+regseason = pd.merge(left=regseason, right=t2pts, how='left', on=['Season', 'T2ID'])
+
+regseason['MPtsScDiff'] = regseason['T1MPtsSc'] - regseason['T2MPtsSc']
+regseason['MPtsTkDiff'] = regseason['T1MPtsTk'] - regseason['T2MPtsTk']
+regseason['WratioDiff'] = regseason['T1Wratio'] - regseason['T2Wratio']
+regseason['WdeltaDiff'] = regseason['T1Wdelta'] - regseason['T2Wdelta']
+
+tourney = pd.merge(left=tourney, right=t1pts, how='left', on=['Season', 'T1ID'])
+tourney = pd.merge(left=tourney, right=t2pts, how='left', on=['Season', 'T2ID'])
+
+tourney['MPtsScDiff'] = tourney['T1MPtsSc'] - tourney['T2MPtsSc']
+tourney['MPtsTkDiff'] = tourney['T1MPtsTk'] - tourney['T2MPtsTk']
+tourney['WratioDiff'] = tourney['T1Wratio'] - tourney['T2Wratio']
+tourney['WdeltaDiff'] = tourney['T1Wdelta'] - tourney['T2Wdelta']
+
+print(tourney.describe())
+
+# ----------------------------- Validation metric -----------------------------
+def val_score(y_pred, y_true):
+    return -(y_true*np.log(y_pred)+(1-y_true)*np.log(1-y_pred)).mean()
 
 
-    
+# --------------------------------- 1st model ---------------------------------
+# tourney based only. based on seed diff
+print("Tourney seeds based model")
 
+data = tourney.loc[:,['Season','T1ID','T2ID','SeedDiff','Result']]
+train1 = data.loc[:,['Season','SeedDiff','Result']]
+train2 = data.loc[:,['Season','SeedDiff','Result']]
+train2['SeedDiff'] *= -1
+train2['Result'] = 1 - train2['Result']
+full_train = pd.concat([train1, train2]).reset_index(drop=True)
+full_test = data[(data['Season']>=2014) & (data['Season']<=2017)]
 
-def get_before(df, year):
-    return df[df['Season']<year]
-
-#%%
+# test each season based on other seasons
+vals = []
 years = [2014,2015,2016,2017]
 for year in years:
-    train = get_before(data, year)
-    X_train = train.loc[:,['SeedDiff']]
+    train = full_train[full_train['Season']!=year]
+    
+    X_train = train[['SeedDiff']]
     y_train = train['Result']
-    X_train, y_train = shuffle(X_train, y_train)
+    X_train, y_train = shuffle(X_train, y_train) 
+    
+    test = full_test[full_test['Season']==year]
+    X_test = test[['SeedDiff']]
+    y_test = test['Result']
 
-    print("training on seeds before", year)
-    print("training on", X_train.shape[0], "samples")
     logreg = LogisticRegression()
     params = {'C': np.logspace(start=-5, stop=3, num=9)}
-    clf = GridSearchCV(logreg, params, cv=5, scoring='neg_log_loss', refit=True)
+    clf = GridSearchCV(logreg, params, cv=10, scoring='neg_log_loss', refit=True)
     clf.fit(X_train, y_train)
-    print('Best log_loss:', clf.best_score_)
-    print('best C:', clf.best_params_['C'])
 
+    y_pred = clf.predict_proba(X_test)[:,1]
+    print("["+str(year)+"] score:", val_score(y_pred,y_test))
+    vals.append(val_score(y_pred,y_test))
+print("[All] score:", 1/4*sum(vals))
+
+
+# --------------------------------- 2nd model ---------------------------------
+# based on prev regular season. Pts scored/taken diff, winrate diff
+print("Reg season stats based model")
+
+data = tourney.loc[:,['Season','T1ID','T2ID','MPtsScDiff','MPtsTkDiff','WratioDiff','WdeltaDiff','Result']]
+train1 = data.loc[:,['Season','MPtsScDiff','MPtsTkDiff','WratioDiff','WdeltaDiff','Result']]
+train2 = data.loc[:,['Season','MPtsScDiff','MPtsTkDiff','WratioDiff','WdeltaDiff','Result']]
+train2['MPtsScDiff'] *= -1
+train2['MPtsTkDiff'] *= -1
+train2['WratioDiff'] *= -1
+train2['WdeltaDiff'] *= -1
+train2['Result'] = 1 - train2['Result']
+full_train = pd.concat([train1, train2]).reset_index(drop=True)
+full_test = data[(data['Season']>=2014) & (data['Season']<=2017)]
+
+#features = ['MPtsScDiff','MPtsTkDiff','WratioDiff']
+features = ['MPtsScDiff','MPtsTkDiff','WdeltaDiff']
+#features = ['MPtsScDiff','MPtsTkDiff']
+#features = ['WratioDiff']
+#features = ['WdeltaDiff']
+
+# test each season based on other seasons
+vals = []
+years = [2014,2015,2016,2017]
+for year in years:
+    train = full_train[full_train['Season']!=year]
     
+    X_train = train[features]
+    y_train = train['Result']
+    X_train, y_train = shuffle(X_train, y_train) 
+    
+    test = full_test[full_test['Season']==year]
+    X_test = test[features]
+    y_test = test['Result']
 
+    logreg = LogisticRegression()
+    params = {'C': np.logspace(start=-5, stop=3, num=9)}
+    clf = GridSearchCV(logreg, params, cv=10, scoring='neg_log_loss', refit=True)
+    clf.fit(X_train, y_train)
 
-
-
-
-
-
-#%% number of w/l and pts scored/taken per season
-pts = regseason.groupby(['Season','WTeamID'])['WScore'].sum().reset_index()
-pts.rename(columns={'WTeamID':'TeamID', 'WScore':'WPtsSc'}, inplace = True)
-pts['LPtsSc'] = regseason.groupby(['Season','LTeamID'])['LScore'].sum().reset_index()['LScore']
-pts['WPtsTk'] = regseason.groupby(['Season','WTeamID'])['LScore'].sum().reset_index()['LScore']
-pts['LPtsTk'] = regseason.groupby(['Season','LTeamID'])['WScore'].sum().reset_index()['WScore']
-pts['Wct'] = regseason.groupby(['Season','WTeamID'])['WScore'].count().reset_index()['WScore']
-pts['Lct'] = regseason.groupby(['Season','LTeamID'])['LScore'].count().reset_index()['LScore']
-
-#%% mean pts scored and win ratio per season
-pts['MPtsSc'] = (pts['WPtsSc'] + pts['LPtsSc']) / (pts['Wct'] + pts['Lct'])
-pts['MPtsTk'] = (pts['WPtsTk'] + pts['LPtsTk']) / (pts['Wct'] + pts['Lct'])
-pts['Wratio'] = pts['Wct'] / (pts['Wct'] + pts['Lct'])
-pts.drop(['WPtsSc','WPtsTk','LPtsSc','LPtsTk','Wct','Lct'], axis=1, inplace=True)
-print(pts.head())
-
-#%% merge pts infos with regseason
-wpts = pts.rename(columns={'TeamID':'WTeamID', 'MPtsSc':'WMPtsSc', 'MPtsTk':'WMPtsTk', 'Wratio':'WWratio'})
-lpts = pts.rename(columns={'TeamID':'LTeamID', 'MPtsSc':'LMPtsSc', 'MPtsTk':'LMPtsTk', 'Wratio':'LWratio'})
-regseason = pd.merge(left=regseason, right=wpts, how='left', on=['Season', 'WTeamID'])
-regseason = pd.merge(left=regseason, right=lpts, how='left', on=['Season', 'LTeamID'])
-print(regseason.head())
-
-
-
-#lmscores = regseason.groupby(['Season','LTeamID'])['LScore'].mean().reset_index().rename(columns={'LScore':'LMScore'})
-#regseason = pd.merge(left=regseason, right=wmscores, how='left', on=['Season', 'WTeamID'])
-#regseason = pd.merge(left=regseason, right=lmscores, how='left', on=['Season', 'LTeamID'])
-#regseason['MScoreDiff'] = regseason['WMScore'] - regseason['LMScore']
-
-#print(regseason[regseason['MScoreDiff']<-16].head(10))
+    y_pred = clf.predict_proba(X_test)[:,1]
+    print("["+str(year)+"] score:", val_score(y_pred,y_test))
+    vals.append(val_score(y_pred,y_test))
+print("[All] score:", 1/4*sum(vals))
